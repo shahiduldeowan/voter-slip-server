536bf1f0f9d8c4fcb3683058b5e5b28ec742894a
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-18 19:10:16 +0600

    add index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// import dotenv from
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import { logger } from "./config/logConfig.js";
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  logger.info(`⚙️ Server is running at port : ${process.env.PORT}`);
+});
