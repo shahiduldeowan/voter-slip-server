7f21078bcdcf1c2b011ecd28fa614bd11375b7ea
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-20 12:33:41 +0600

    handle database query error

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,13 @@
 import { ConnectionPool } from "mssql";
 import config from "../config/dbConfig.js";
+import { logger } from "../config/logConfig.js";
 
 const poolPromise = new ConnectionPool(config);
 
 const connectToDB = async () => poolPromise.connect();
+
+poolPromise.on("error", (err) => {
+  logger.error(`Database connection failed ${err.message}`);
+});
 
 export { connectToDB, poolPromise };
