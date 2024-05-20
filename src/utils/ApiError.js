78c084f2b57a74fedc2cc2f8317ec600cc5a6d0d
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-20 15:31:49 +0600

    declare ApiError response class

---
+++ src/utils/ApiError.js
@@ -0,0 +1,31 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went error",
+    error = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.error = error;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+class ApiJsonError {
+  constructor(statusCode, message = "Success") {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
+
+export { ApiError, ApiJsonError };
