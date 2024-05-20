383bd8b13ac0bd1ae6dbe75a90423cba694a1fd1
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-20 14:20:09 +0600

    call database connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
 import { logger } from "./config/logConfig.js";
+import { connectToDatabase } from "./db/index.js";
 dotenv.config({
   path: `.env.${process.env.NODE_ENV}`,
 });
+
 const PORT = process.env.PORT || 8000;
+
+// Database Connection
+connectToDatabase()
+  .then(() => {
+    logger.info(`Database connection successful`);
+  })
+  .catch((err) => {
+    logger.error(`Database connection failed ${err.message}`);
+  });
 
 app.listen(PORT, () => {
   logger.info(`⚙️ Server is running at port : ${process.env.PORT}`);
