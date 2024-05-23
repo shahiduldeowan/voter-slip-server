3f9b1f99c46ff535d457e9aa32a94d8ebae1ca10
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-23 12:34:09 +0600

    add userController

---
+++ src/controllers/v1/userControllers.js
@@ -0,0 +1,115 @@
+import bcrypt from "bcrypt";
+import { logger } from "../../config/logConfig.js";
+import { USER_ACTIONS } from "../../constants.js";
+import {
+  generateToken,
+  isPasswordCorrect,
+  onLoginUser,
+} from "../../models/v1/userModels.js";
+import { ApiJsonError } from "../../utils/ApiError.js";
+import { ApiResponse } from "../../utils/ApiResponse.js";
+
+const existUser = async (req, res) => {
+  const { username } = req.params;
+  res.send({
+    isExist: true,
+    username,
+  });
+};
+
+const registerUser = async (req, res) => {
+  const { name } = req.body;
+  const encodedName = await bcrypt.hash(name, 10);
+  const compareName = await bcrypt.compare(
+    name,
+    "$2b$10$jJebbOMRmYZXAr40UAiDu.1jv5yMio16pqNgMhYy13XbRQTEGE6h6"
+  );
+  res.send({
+    encodedName,
+    compareName,
+    message: "User registered successfully",
+  });
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const body = req.body;
+    const username = body?.Username;
+    const password = body?.Password;
+
+    if (!username && !password) {
+      return res
+        .status(401)
+        .json(new ApiJsonError(401, "Username or password is required!"));
+    }
+
+    const findUser = await onLoginUser(USER_ACTIONS.LOGIN_FIND, username);
+    if (!findUser) {
+      return res
+        .status(404)
+        .json(new ApiJsonError(404, "User does not exist!"));
+    }
+
+    if (!findUser[0].Password) {
+      return res
+        .status(404)
+        .json(new ApiJsonError(404, "User does not exist!"));
+    }
+
+    const isValidPassword = await isPasswordCorrect(
+      password,
+      findUser[0].Password
+    );
+
+    if (!isValidPassword) {
+      return res
+        .status(401)
+        .json(new ApiJsonError(401, "Username or password does not match"));
+    }
+
+    const user = await onLoginUser(USER_ACTIONS.LOGIN, username);
+
+    if (!user) {
+      return res
+        .status(401)
+        .json(new ApiJsonError(401, "Username or password does not match"));
+    }
+
+    if (!user[0]?.UserID) {
+      return res
+        .status(401)
+        .json(new ApiJsonError(401, "Username or password does not match"));
+    }
+
+    const token = await generateToken(user[0]?.UserID);
+    const newUser = {
+      token,
+      ...user,
+    };
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    return res
+      .status(200)
+      .cookie("token", token, options)
+      .json(new ApiResponse(200, newUser, "Login successful"));
+  } catch (error) {
+    // logger.error(error);
+    logger.error(error);
+    console.log(error.stack);
+
+    return res
+      .status(error.status || 500)
+      .json(
+        new ApiJsonError(
+          error.status || 500,
+          error.message || "Internal server error!"
+        )
+      );
+  }
+};
+
+export { existUser, loginUser, registerUser };
