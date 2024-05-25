6eaa9991e2bdef0bc739287b781f6ddfd95962df
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-25 16:17:28 +0600

    add auth middleware in logout route

--- src/routes/v1/userRoutes.js
+++ src/routes/v1/userRoutes.js
@@ -5,12 +5,13 @@ import {
   logoutUser,
   registerUser,
 } from "../../controllers/v1/userControllers.js";
+import { verifyJWT } from "../../middlewares/v1/authMiddleware.js";
 
 const router = Router();
 
 router.route("/exist/:username").get(existUser);
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/logout").post(logoutUser);
+router.route("/logout").get(verifyJWT, logoutUser);
 
 export default router;
