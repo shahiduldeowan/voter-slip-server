4f81d1620949acfa0c1b46b5df45e2cd5308f6da
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-27 13:51:04 +0600

    add member route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRoutes from "./userRoutes.js";
+import memberRoutes from "./memberRoutes.js";
 
 const router = Router();
 
 router.use("/user", userRoutes);
+router.use("/member", memberRoutes);
 
 export default router;
