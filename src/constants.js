c67cba37878b13a8c218a97f31182b0cdbeaf0dd
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-27 13:49:09 +0600

    rename DB_ACTIONS instead of USER_ACTIONS

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 const DB_NAME = "VoterSlipDB";
 
-const USER_ACTIONS = {
+const DB_ACTIONS = {
   LOGIN_FIND: "USER_FIND",
   LOGIN: "USER_LOGIN",
   GET: "GET",
 };
 
-export { DB_NAME, USER_ACTIONS };
+export { DB_ACTIONS, DB_NAME };
