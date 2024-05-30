fe90a174e8ea98491e576bf97de005309cbffbb6
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-30 18:57:41 +0600

    add uploadMembersImagesZipFile route

--- src/routes/v1/memberRoutes.js
+++ src/routes/v1/memberRoutes.js
@@ -2,15 +2,23 @@ import { Router } from "express";
 import {
   getMembers,
   uploadMembers,
+  uploadMembersImagesZipFile,
 } from "../../controllers/v1/memberControllers.js";
 import { verifyJWT } from "../../middlewares/v1/authMiddleware.js";
 import { uploadFile } from "../../middlewares/v1/multerMiddleware.js";
 
 const router = Router();
 
+router.route("/").get(verifyJWT, getMembers);
 router
   .route("/upload")
   .post(verifyJWT, uploadFile.single("members"), uploadMembers);
-router.route("/").get(verifyJWT, getMembers);
+router
+  .route("/upload-images-zip")
+  .post(
+    verifyJWT,
+    uploadFile.single("members-images-zip-file"),
+    uploadMembersImagesZipFile
+  );
 
 export default router;
