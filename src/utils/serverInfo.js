8ae95d16b584228b656fa059c35aac754f7eff65
Shahidul Deowan <deowanshahidul@gmail.com>
2024-05-30 18:58:15 +0600

    add getServerIP function

---
+++ src/utils/serverInfo.js
@@ -0,0 +1,16 @@
+import os from "os";
+
+const getServerIP = () => {
+  const interfaces = os.networkInterfaces();
+  for (const interfaceName in interfaces) {
+    for (const iface of interfaces[interfaceName]) {
+      if (iface.family === "IPv4" && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+
+  return "localhost";
+};
+
+export { getServerIP };
