f8f86a7d6249e633d712eb0b706b521166f4588e
Shahidul Deowan <deowanshahidul@gmail.com>
2024-06-04 14:43:44 +0600

    Refactored and documented onSlipIssue and onSlipQueue functions

--- src/models/v1/slipModels.js
+++ src/models/v1/slipModels.js
@@ -1,4 +1,6 @@
+import { DB_ACTIONS } from "../../constants.js";
 import { poolPromise } from "../../db/index.js";
+import { getServerIP } from "../../utils/serverInfo.js";
 
 /**
  * This function is responsible for inserting a new record into the voters slip table.
@@ -20,10 +22,50 @@ const onSlipIssue = async (userID, voterAccountNumber) => {
 
   const result = request.recordset;
   if (result && result.length > 0) {
-    return request[0]?.Message || "Failed to slip issue";
+    return result[0];
   }
 
-  return "Failed to slip issue";
+  return null;
+};
+
+/**
+ * This function retrieves a list of voter slips that are currently in the queue.
+ * It connects to the database, executes a stored procedure, and maps the result to a list of objects.
+ *
+ * @returns {Promise<Array>} - A promise that resolves to an array of objects representing the voter slips in the queue.
+ * Each object contains the following properties: Name, AccountNumber, PhotoURL, Counter, and IssueDate.
+ * If there are no voter slips in the queue, the promise resolves to an empty array.
+ */
+const onSlipQueue = async () => {
+  const request = await poolPromise
+    .request()
+    .input("ActionName", DB_ACTIONS.SLIP_QUEUE)
+    .execute("dbo.sp_VoterSlips_Select");
+
+  const result = request.recordset;
+  if (result && result.length > 0) {
+    const serverIP = getServerIP();
+    const port = process.env.PORT || 8000;
+
+    const queueList = result?.map((queue) => {
+      const photoURL = queue?.PhotoURL;
+      const genPhotoURL = photoURL
+        ? `http://${serverIP}:${port}/${photoURL}`
+        : photoURL;
+
+      return {
+        Name: queue.Name,
+        AccountNumber: queue.AccountNumber,
+        PhotoURL: genPhotoURL,
+        Counter: queue.Counter,
+        IssueDate: queue.IssueDate,
+      };
+    });
+
+    return queueList;
+  }
+
+  return [];
 };
 
 export { onSlipIssue };
