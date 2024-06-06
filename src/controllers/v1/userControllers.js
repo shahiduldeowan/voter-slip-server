6fa677236992a2f962fbb56d20f4fe5c36889f33
Shahidul Deowan <deowanshahidul@gmail.com>
2024-06-06 13:20:46 +0600

    implement register user and isExist user also add getAllUsers

--- src/controllers/v1/userControllers.js
+++ src/controllers/v1/userControllers.js
@@ -1,37 +1,100 @@
-import bcrypt from "bcrypt";
 import { logger } from "../../config/logConfig.js";
 import { COOKIE_OPTIONS, DB_ACTIONS } from "../../constants.js";
 import {
+  generateHashPassword,
   generateToken,
   isPasswordCorrect,
+  isUserExistInDB,
+  onGetAllUsers,
   onGetUser,
   onLoginUser,
   onLogoutUser,
+  onRegisterUser,
 } from "../../models/v1/userModels.js";
 import { ApiJsonError } from "../../utils/ApiError.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
 import { getOSByReq, getReqIpAddressByReq } from "../../utils/requestInfo.js";
 
 const existUser = async (req, res) => {
-  const { username } = req.params;
-  res.send({
-    isExist: true,
-    username,
-  });
+  try {
+    const { username } = req.params;
+    const isExistUser = await isUserExistInDB(username);
+    return res.status(200).json(
+      200,
+      {
+        isExistUser: isExistUser === 1 ? true : false,
+      },
+      isExistUser === 1 ? "User exist" : "User not exist"
+    );
+  } catch (error) {
+    logger.error(error.message || "Internal server error");
+    console.log(error.stack);
+    return res.status(500).json(new ApiJsonError(500, "Internal server error"));
+  }
 };
 
 const registerUser = async (req, res) => {
-  const { name } = req.body;
-  const encodedName = await bcrypt.hash(name, 10);
-  const compareName = await bcrypt.compare(
-    name,
-    "$2b$10$jJebbOMRmYZXAr40UAiDu.1jv5yMio16pqNgMhYy13XbRQTEGE6h6"
-  );
-  res.send({
-    encodedName,
-    compareName,
-    message: "User registered successfully",
-  });
+  try {
+    const { Username, Password, FirstName, LastName, Email, RoleID } = req.body;
+
+    if (!Username && !Password) {
+      return res
+        .status(404)
+        .json(new ApiJsonError(404, "Username or password is required!"));
+    }
+
+    if (!FirstName && !LastName) {
+      return res
+        .status(404)
+        .json(new ApiJsonError(404, "First name or last name is required!"));
+    }
+
+    if (!RoleID) {
+      return res
+        .status(404)
+        .json(new ApiJsonError(404, "Role ID is required!"));
+    }
+
+    const isExistUser = await isUserExistInDB(Username);
+    if (isExistUser) {
+      return res.status(404).json(new ApiJsonError(404, "User already exist!"));
+    }
+
+    const hashPassword = await generateHashPassword(Password);
+
+    const newUser = {
+      UserID: req.user?.UserID,
+      Username,
+      Password: hashPassword,
+      FirstName,
+      LastName,
+      Email,
+      RoleID,
+    };
+
+    const registerUserID = await onRegisterUser(newUser);
+
+    if (!registerUserID) {
+      return res
+        .status(500)
+        .json(new ApiJsonError(500, "Error registering user"));
+    }
+
+    return res.status(200).json(
+      new ApiResponse(
+        200,
+        {
+          UserID: registerUserID,
+        },
+        "User registered successfully"
+      )
+    );
+  } catch (error) {
+    logger.error(error.message || "Error registering user");
+    console.log(error.stack);
+
+    res.status(500).json(new ApiJsonError(500, "Error registering user"));
+  }
 };
 
 const loginUser = async (req, res) => {
@@ -152,6 +215,26 @@ const authUserStatus = async (req, res) => {
   }
 };
 
+const getAllUsers = async (req, res) => {
+  try {
+    const allUsers = await onGetAllUsers();
+
+    return res.status(200).json(new ApiResponse(200, allUsers, "User found"));
+  } catch (error) {
+    logger.error(error.message || "Internal server error!");
+    console.log(error.stack);
+
+    res
+      .status(error.status || 500)
+      .json(
+        new ApiJsonError(
+          error.status || 500,
+          error.message || "Internal server error!"
+        )
+      );
+  }
+};
+
 const logoutUser = async (req, res) => {
   try {
     const userID = req?.user?.UserID;
@@ -183,4 +266,11 @@ const logoutUser = async (req, res) => {
   }
 };
 
-export { authUserStatus, existUser, loginUser, logoutUser, registerUser };
+export {
+  authUserStatus,
+  existUser,
+  getAllUsers,
+  loginUser,
+  logoutUser,
+  registerUser,
+};
