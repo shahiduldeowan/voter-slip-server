ba07a701f144679b8f24a575174d57128494826e
Shahidul Deowan <deowanshahidul@gmail.com>
2024-06-06 13:16:36 +0600

    create socket server

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
+import { createServer } from "http";
+import { Server as socketIO } from "socket.io";
+import morganMiddleware from "./middlewares/morganMiddleware.js";
 
 const app = express();
+const server = createServer(app);
+const io = new socketIO(server, {
+  cors: {
+    origin: ["http://localhost:5173", "http://localhost:4173"],
+    credentials: true,
+  },
+});
+
+app.set("io", io);
 
 app.use(
   cors({
@@ -15,13 +27,18 @@ app.use(express.static("public"));
 app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 
+app.use(morganMiddleware);
+
 // Imports
 import { errorHandler } from "./middlewares/errorMiddlewares.js";
 import healthCheckRouter from "./routes/healthCheckRoutes.js";
 import v1Router from "./routes/v1/index.js";
+import { initializeSocketIO } from "./socket/index.js";
 
 app.use("/", healthCheckRouter);
 app.use("/api/v1", v1Router);
+
+initializeSocketIO(io);
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -33,4 +50,4 @@ app.use((req, res, next) => {
 // Error handler
 app.use(errorHandler);
 
-export { app };
+export { server };
