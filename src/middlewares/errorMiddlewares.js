84a7d61351f7d3520bc091bbc3d38c1600263439
Shahidul Deowan <deowanshahidul@gmail.com>
2024-06-06 13:21:19 +0600

    enhance

--- src/middlewares/errorMiddlewares.js
+++ src/middlewares/errorMiddlewares.js
@@ -1,9 +1,28 @@
 import { logger } from "../config/logConfig.js";
-import { ApiJsonError } from "../utils/ApiError.js";
+import { ApiError, ApiJsonError } from "../utils/ApiError.js";
 
 const errorHandler = (err, _, res, next) => {
   logger.error(err.message || "Internal server error!");
   res.status(500).json(new ApiJsonError(500, "Internal server error"));
 };
 
-export { errorHandler };
+const errorsHandler = (err, req, res, next) => {
+  let error = err;
+  if (!(error instanceof ApiError)) {
+    const statusCode = error.statusCode ? 400 : 500;
+    const message = error.message || "Something went wrong";
+    error = new ApiError(statusCode, message, error?.errors || [], err.stack);
+  }
+
+  const response = {
+    ...error,
+    message: error.message,
+    ...(process.env.NODE_ENV === "development" ? { stack: error.stack } : {}),
+  };
+
+  logger.error(`${error.message}`);
+
+  return res.status(error.statusCode).json(response);
+};
+
+export { errorHandler, errorsHandler };
